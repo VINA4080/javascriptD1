5e23cb3386db6265b73fcb64b085b28730976979
VINA4080 <vinaandrianarisoa1@gmail.com>
2022-08-16 09:47:22 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+var color = "red";
+let name = "rasoa";
+const vitesse = "18Km/h";
+color = blue;
+name = "rabe"
+
+/* function preparing(){
+    // console.log("eating");
+    return("eating");
+}
+function addition (a, b) {
+    return a+b;
+}
+function soustraction (d, b){
+    return d-b;
+}
+function division (d, f){
+    return d/f;
+}
+function multiplication (d, c){
+    return d*c;
+}
+console.log(multiplication(6,3));
+console.log(division(6,3));
+console.log(addition(6,3));
+console.log(preparing());
+console.log(color, name, vitesse);
+// if, while,(switch case)*/
+// if("blue" == "red"){
+// console.log()
+//}
+
+function calculatrice (signe, a, b){
+    if("signe==addition"){
+        return a + b;
+    }
+    if ("signe==soustraction"){
+        return a - b;
+    }
+    if("sign==division"){
+        return a / b;
+    }
+    console.log(calculatrice());
+}
