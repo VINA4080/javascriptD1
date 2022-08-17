9bcb5cccd3d8ac23115bd1fb31063181ba32b7d1
VINA4080 <vinaandrianarisoa1@gmail.com>
2022-08-17 08:31:57 +0300

    Javascript

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var color = "red";
-let name = "rasoa";
-const vitesse = "18Km/h";
-color = blue;
-name = "rabe"
+// var color = "red";
+// let name = "rasoa";
+// const vitesse = "18Km/h";
+// color = blue;
+// name = "rabe"
 
 /* function preparing(){
     // console.log("eating");
@@ -30,18 +30,98 @@ console.log(color, name, vitesse);
 // console.log()
 //}
 
-function calculatrice (signe, a, b){
-    if(signe == "addition"){
-        return a + b;
-    }
-    if (signe == "soustraction"){
-        return a - b;
-    }
-    if(signe == "division"){
-        return a / b;
-    }
-    if(signe = "multiplication"){
-        return a * b;
-    }
-    console.log(calculatrice());
-}
+// function calculatrice (signe, a, b){
+//     if(signe == "addition"){
+//         return a + b;
+//     }
+//     if (signe == "soustraction"){
+//         return a - b;
+//     }
+//     if(signe == "division"){
+//         return a / b;
+//     }
+//     if(signe = "multiplication"){
+//         return a * b;
+//     }
+//     console.log(calculatrice());
+// }
+
+
+// var i = 0;
+
+// ------------------------------DO WHILE & SWITCH-------------------------
+
+// do {
+//     i++;
+//     console.log("Bonjour");
+// } while (i < 10);
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`bonjour numéro: ${i}`);
+// }
+
+// let number = 4 ;
+
+// switch (number) {
+
+//     case 1: 
+//         console.log("Nombre 1");
+//         break;
+//     case 2: 
+//         console.log("Nombre 2");
+//         break;
+//     case 3: 
+//         console.log("Nombre 3");
+//         break;
+//     case 4: 
+//         console.log("Nombre 4");
+//         break;
+//     default:
+//         console.log("Nombre invalide");
+//         break;
+// }
+
+// --------------------------------Tableau-------------------------------------
+// length = taille du tableau
+// pop = effacer le dernier
+// shift = effacer le debut
+// join = chaine de caractère
+// push = inserer un élément
+// table = autre moyen d'afficher un tableau
+// includes = verifier un élément dans un tableau
+
+
+// var nom = ['Vina' , 'Lala' , 'Rasoa' , 'Koto'];
+// for (let pointer = 0; pointer < nom.length; pointer++){
+//     console.log(pointer);
+//     console.log(nom[pointer]);
+// }
+
+// var nom = ['Vina' , 'Lala' , 'Rasoa' , 'Koto'];
+// for (let pointer = nom.pop-1 ; pointer >= 0; pointer--){
+//     console.log(pointer);
+//     console.log(nom[pointer]);
+// }
+
+// var nom = ['Vina' , 'Lala' , 'Rasoa' , 'Koto'];
+//     nom.pop();
+//     console.log(nom);
+//     console.log(nom.pop());
+
+// var nom = ['Vina' , 'Lala' , 'Rasoa' , 'Koto'];
+//     nom.shift();
+// console.log(nom);
+
+// var nom = ['Vina' , 'Lala' , 'Rasoa' , 'Koto'];
+//     console.log(nom.join("/"));
+
+// var nom = ['Vina' , 'Lala' , 'Rasoa' , 'Koto'];
+//     nom.push("Ketaka");
+//     console.log(nom);
+
+// var nom = ['Vina' , 'Lala' , 'Rasoa' , 'Koto'];
+//     console.table(nom);
+
+var nom = ['Vina' , 'Lala' , 'Rasoa' , 'Koto'];
+    console.log(nom.includes("Vina"));
+
