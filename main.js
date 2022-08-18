62001a92b4904b39b70368c2e090e867725f3ead
VINA4080 <vinaandrianarisoa1@gmail.com>
2022-08-18 16:40:07 +0300

    test change

--- main.js
+++ main.js
@@ -81,7 +81,7 @@ console.log(color, name, vitesse);
 //         break;
 // }
 
-// --------------------------------Tableau-------------------------------------&
+// --------------------------------Tableau-------------------------------------
 // length = taille du tableau
 // pop = effacer le dernier
 // shift = effacer le debut
